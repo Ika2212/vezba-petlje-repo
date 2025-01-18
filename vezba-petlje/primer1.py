54c794c3aed9e04d08579533561e7aaf7e85999a
Ika2212 <ikamilenovic@gmail.com>
2025-01-18 14:54:08 +0100

    dodat primer

---
+++ vezba-petlje/primer1.py
@@ -0,0 +1,16 @@
+import time
+pocetna_pozicija = 0
+cilj = 50
+trenutna_pozicija = 0
+brzina = 10
+for x in range(10):
+    print(trenutna_pozicija)
+    if trenutna_pozicija == cilj:
+        print("stigao do cilja")
+        break
+    elif trenutna_pozicija > cilj:
+        print("Prosli ste") 
+    elif trenutna_pozicija < cilj:
+        print("niste jos stigli")
+    trenutna_pozicija += brzina 
+    time.sleep(1)
